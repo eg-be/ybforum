a62dc5d99a96b334ebe74e747d0456a2bdce5f43
eg <23149233+eg-be@users.noreply.github.com>
2018-01-19 07:27:19 +0000

    Remove the '2.0' from all titles and texts. Its the one and only 'YB Forum'

--- web/user/index.php
+++ web/user/index.php
@@ -98,7 +98,7 @@ catch(Exception $ex)
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="../ybforum.css">
-        <title>YB Forum 2.0</title>
+        <title>YB Forum</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
