6ef388820993e3d4751ecb28b096a7d21d5813c3
eg <eg@17607161-be10-47fc-b9c0-b4dc995f904b>
2018-01-29 08:15:17 +0000

    Add more notes about hotmail addresses behaving crappy

--- trunk/web/migrateuser.php
+++ trunk/web/migrateuser.php
@@ -136,7 +136,13 @@ catch(Exception $ex)
                     . 'Hotmail.com und outlook.com haben die Angewohnheit Emails, insbesondere solche von 1898.ch, ohne weitere Benachrichtigung zu '
                     . '<a href="https://www.google.ch/search?q=hotmail+silently+discards+emails" target="_blank">löschen</a>. '
                     . 'Hotmail/Outlook Benutzer müssen 1898.ch <span class="fbold">vor</span> '
-                    . 'der Registrierung zur Liste der vertrauenswürdigen Absender hinzufügen.'
+                    . 'der Registrierung zur Liste der vertrauenswürdigen Absender hinzufügen. '
+                    . 'Anschliessend muss mindestens eine halbe Stunde gewartet werden, bis die Migration vorgenommen wird, '
+                    . 'damit die Einstellungen von hotmail übernommen werden.'
+                    . '</div>';
+                echo '<div class="fullwidthcenter fbold">'
+                    . 'Alternativ empfiehlt es sich für den Stammposter-Account gänzlich auf hotmail.com / outlook.com '
+                    . 'Adressen zu verzichten und eine andere <span class="fitalic">(gültige)</span> Mailadresse anzugeben.'
                     . '</div>';
             }
             else
