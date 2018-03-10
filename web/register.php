ba01667917761e846bd57a717ae6d61c55b6a4b0
eg <23149233+eg-be@users.noreply.github.com>
2018-03-10 22:56:08 +0000

    remove note about hotmail, hotmail is blacklisted

--- web/register.php
+++ web/register.php
@@ -166,13 +166,6 @@ catch(Exception $ex)
                 </table>
             </form>
         </div>
-        <div class="fullwidthcenter">
-            <span class="fbold">Achtung:</span> 
-            Hotmail.com und outlook.com haben die Angewohnheit Emails, insbesondere solche von 1898.ch, ohne weitere Benachrichtigung zu 
-            <a href="https://www.google.ch/search?q=hotmail+silently+discards+emails" target="_blank">löschen</a>. 
-            Hotmail/Outlook Benutzer müssen 1898.ch <span class="fbold">vor</span> 
-            der Registrierung zur Liste der vertrauenswürdigen Absender hinzufügen.
-        </div>
         <?php
         if($registerUserHandler && !$registerUserHandler->HasException())
         {
