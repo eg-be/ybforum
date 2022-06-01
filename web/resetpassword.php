cf999cae55deed23e7b49ac236f1acd1cd7c30b3
Elias Gerber <23149233+eg-be@users.noreply.github.com>
2022-06-01 09:25:21 +0200

    proper including

--- web/resetpassword.php
+++ web/resetpassword.php
@@ -25,12 +25,11 @@ require_once __DIR__.'/handlers/ConfirmHandlerFactory.php';
 require_once __DIR__.'/handlers/ConfirmResetPasswordHandler.php';
 require_once __DIR__.'/handlers/UpdatePasswordHandler.php';
 require_once __DIR__.'/pageparts/ResetPasswordForm.php';
-require_once __DIR__.'/pageparts/StandWithUkr.php';
 ?>
 
 <html lang="de-ch">
     <head>
-        <link rel="stylesheet" type="text/css" href="ybforum.css">        
+        <link rel="stylesheet" type="text/css" href="ybforum.css?v=r182">        
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="theme-color" content="#FFCC00">
