b114d2116a6854781a22245efa8fddd0827d5415
Elias Gerber <23149233+eg-be@users.noreply.github.com>
2022-06-06 21:28:49 +0200

    add some dummy thread and posts

---
+++ test/posts.sql
@@ -0,0 +1,35 @@
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(1, 1, NULL, 101, 'Thread 1', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:31:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(2, 2, NULL, 102, 'Thread 2', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:32:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(3, 3, NULL, 103, 'Thread 3', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:33:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(4, 4, NULL, 101, 'Thread 4', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:34:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(5, 5, NULL, 102, 'Thread 5', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:35:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(6, 6, NULL, 103, 'Thread 6', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:36:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(7, 7, NULL, 101, 'Thread 7', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:37:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(8, 8, NULL, 102, 'Thread 8', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:38:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(9, 9, NULL, 103, 'Thread 9', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:39:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(10, 10, NULL, 101, 'Thread 10', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:40:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(11, 11, NULL, 102, 'Thread 11', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:41:00', '::1');
+
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(12, 12, NULL, 103, 'Thread 12', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:42:00', '::1');
