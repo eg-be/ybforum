268a156c7bae24caa63b8f8f47e188fdb9b5ef32
Elias Gerber <23149233+eg-be@users.noreply.github.com>
2022-06-06 21:52:35 +0200

    minimal tree for test data

--- test/ForumDbTest.php
+++ test/ForumDbTest.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
+require_once __DIR__.'/BaseTest.php';
 require_once __DIR__.'/../web/model/ForumDb.php';
 
 /**
@@ -11,43 +12,17 @@ require_once __DIR__.'/../web/model/ForumDb.php';
  * to setup the test-database.
  * 
  */
-final class ForumDbTest extends TestCase
+final class ForumDbTest extends BaseTest
 {
     private $db;
 
-    const TEST_DB = [
-        __DIR__.'/../database/dbybforum-no-data.dump.sql',
-        __DIR__.'/../database/log_type_table_data.dump.sql',
-        __DIR__.'/users.sql'
-    ];
-
-    public static function setUpBeforeClass(): void
-    {
-        // restore an empty database for the tests
-        foreach(ForumDbTest::TEST_DB as $file)
-        {
-            $cmd = sprintf('mysql -h localhost -u %s -p%s %s < %s 2>&1', 
-            DbConfig::RW_USERNAME, DbConfig::RW_PASSWORD, DbConfig::DEFAULT_DB, $file);
-            $output = null;
-            $result_code = null;
-            fwrite(STDOUT, 'Executing: ' . $cmd . PHP_EOL);
-            $res = exec($cmd, $output, $result_code);
-            if($res === false || $result_code !== 0)
-            {
-                throw new Exception('Failed to init test-datase: ' . implode(PHP_EOL, $output));
-            }
-            foreach($output as $res)
-            {
-                fwrite(STDOUT, $res . PHP_EOL);
-            }
-        }
-    }
-
     protected function setUp(): void
     {
+        // some of the tests will modify the db, 
+        // just re-create from scratch on every test
+        BaseTest::createTestDatabase();        
         $this->db = new ForumDb();
     }
-
 
     protected function assertPreConditions(): void
     {
@@ -66,18 +41,18 @@ final class ForumDbTest extends TestCase
     public function testGetUserCount() : void
     {
         $count = $this->db->GetUserCount();
-        $this->assertEquals(7, $count);
+        $this->assertEquals(8, $count);
     }
 
     public function testGetThreadCount() : void
     {
         $count = $this->db->GetThreadCount();
-        $this->assertEquals(0, $count);
+        $this->assertEquals(12, $count);
     }
 
     public function testGetPostCount() : void
     {
         $count = $this->db->GetPostCount();
-        $this->assertEquals(0, $count);
+        $this->assertEquals(19, $count);
     }
 }
--- test/posts.sql
+++ test/posts.sql
@@ -1,35 +1,43 @@
+/* just some very simple thread indexes */
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(1, 1, NULL, 101, 'Thread 1', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:31:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(2, 2, NULL, 102, 'Thread 2', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:32:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(3, 3, NULL, 103, 'Thread 3', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:33:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(4, 4, NULL, 101, 'Thread 4', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:34:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(5, 5, NULL, 102, 'Thread 5', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:35:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(6, 6, NULL, 103, 'Thread 6', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:36:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(7, 7, NULL, 101, 'Thread 7', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:37:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(8, 8, NULL, 102, 'Thread 8', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:38:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(9, 9, NULL, 103, 'Thread 9', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:39:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(10, 10, NULL, 101, 'Thread 10', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:40:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(11, 11, NULL, 102, 'Thread 11', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:41:00', '::1');
-
 INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
 VALUES(12, 12, NULL, 103, 'Thread 12', 'The quick brown fox jumps over the lazy dog', 1, 0, '2020-03-30 14:42:00', '::1');
+
+/* populate ONE thred with some post-tree. That is enough for most of the tests.
+   tests requiring more data, shall provide their own data and load it */
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(20, 3, 3, 101, 'Thread 3 - A1', 'The quick brown fox jumps over the lazy dog', 2, 1, '2020-03-30 14:50:00', '::1');
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(21, 3, 20, 102, 'Thread 3 - A1-1', 'The quick brown fox jumps over the lazy dog', 3, 2, '2020-03-30 14:51:00', '::1');
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(22, 3, 3, 103, 'Thread 3 - A2', 'The quick brown fox jumps over the lazy dog', 7, 1, '2020-03-30 14:52:00', '::1');
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(23, 3, 20, 102, 'Thread 3 - A1-2', 'The quick brown fox jumps over the lazy dog', 4, 2, '2020-03-30 14:53:00', '::1');
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(24, 3, 22, 101, 'Thread 3 - A2-1', 'The quick brown fox jumps over the lazy dog', 8, 2, '2020-03-30 14:54:00', '::1');
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(25, 3, 23, 101, 'Thread 3 - A1-2-1', 'The quick brown fox jumps over the lazy dog', 5, 3, '2020-03-30 14:55:00', '::1');
+INSERT INTO post_table (idpost, idthread, parent_idpost, iduser, title, content, `rank`, indent, creation_ts, ip_address)
+VALUES(26, 3, 20, 103, 'Thread 3 - A1-3', 'The quick brown fox jumps over the lazy dog', 6, 2, '2020-03-30 14:55:00', '::1');
+
