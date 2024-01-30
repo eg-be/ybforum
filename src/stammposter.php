b58302873241d444b8b42e4983871a70f7096cef
Elias Gerber <23149233+eg-be@users.noreply.github.com>
2024-01-30 20:22:59 +0100

    issue-85-fix-favicon

--- src/stammposter.php
+++ src/stammposter.php
@@ -92,6 +92,7 @@ catch(Exception $ex)
 <html lang="de-ch">
     <head>
         <link rel="stylesheet" type="text/css" href="ybforum.css?v=r183">
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
         <title>YB Forum</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
