93670c9ca9f496d2805d20968564abcacd15184f
Elias Gerber <23149233+eg-be@users.noreply.github.com>
2024-02-14 22:17:16 +0100

    issue #63 some more tests

--- test/helpers/LoggerTest.php
+++ test/helpers/LoggerTest.php
@@ -75,4 +75,106 @@ final class LoggerTest extends BaseTest
         $this->assertNotEquals($id1, $id2);
         $this->assertNotEquals($id2, $id3);
     }
+
+    public function testLogMessage(): void
+    {
+        // just log a test-message without ext-info
+        // some properties are set from INPUT_SERVER, what is probably not so nice
+        // todo: setting it here does not work, whats wrong?
+        //$_SERVER['REMOTE_ADDR'] = '1.1.1.1';
+        //$_SERVER['REQUEST_URI'] = 'some-uri';
+
+        $l = new Logger($this->db);
+        $l->LogMessage(LogType::LOG_AUTH_FAILED_NO_SUCH_USER, "testLogMessage-msg1");
+
+        // get the id to compare later
+        $id1 = $l->GetLogTypeId(LogType::LOG_AUTH_FAILED_NO_SUCH_USER);
+
+        // read back the values
+        $query = 'SELECT idlog, idlog_type, ts, iduser, historic_user_context, message, request_uri, ip_address, admin_iduser '
+        . 'FROM log_table '
+        . 'ORDER BY idlog DESC';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $this->assertTrue($stmt->rowCount() >= 1);
+        $result = $stmt->fetch();
+        $this->assertNotNull($result);
+        $this->assertSame($id1, $result['idlog_type']);
+        $this->assertNotNull($result['ts']);
+        $this->assertNull($result['iduser']);
+        $this->assertNull($result['historic_user_context']);
+        $this->assertSame('testLogMessage-msg1', $result['message']);
+        //$this->assertSame('some-uri', $result['request_uri']);
+        //$this->assertSame('some-ip', $result['ip_address']);
+    }
+
+    public function testLogMessageWithExtInfo(): void
+    {
+        // some properties are set from INPUT_SERVER, what is probably not so nice
+        // todo: setting it here does not work, whats wrong?
+        //$_SERVER['REMOTE_ADDR'] = '1.1.1.1';
+        //$_SERVER['REQUEST_URI'] = 'some-uri';
+
+        $l = new Logger($this->db);
+        $l->LogMessage(LogType::LOG_AUTH_FAILED_NO_SUCH_USER, "testLogMessageWithExtInfo-msg1", "testLogMessageWithExtInfo-ext1");
+
+        // get the id to compare later
+        $id1 = $l->GetLogTypeId(LogType::LOG_AUTH_FAILED_NO_SUCH_USER);
+
+        // read back the values
+        $query = 'SELECT idlog, idlog_type, ts, iduser, historic_user_context, message, request_uri, ip_address, admin_iduser '
+        . 'FROM log_table '
+        . 'ORDER BY idlog DESC';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $this->assertTrue($stmt->rowCount() >= 1);
+        $result = $stmt->fetch();
+        $this->assertNotNull($result);
+        $this->assertSame($id1, $result['idlog_type']);
+        $this->assertNotNull($result['ts']);
+        $this->assertNull($result['iduser']);
+        $this->assertNull($result['historic_user_context']);
+        $this->assertSame('testLogMessageWithExtInfo-msg1', $result['message']);
+
+        $idLog = $result['idlog'];
+        $query = 'SELECT info '
+        . 'FROM log_extended_info '
+        . 'WHERE idlog = :idlog';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute(array(':idlog' => $idLog));
+        $this->assertTrue($stmt->rowCount() == 1);
+        $result = $stmt->fetch();
+        $this->assertNotNull($result);
+        $this->assertEquals('testLogMessageWithExtInfo-ext1', $result['info']);
+    }
+
+    public function testLogMessageWithUserId(): void
+    {
+        // just log a test-message with some user-info
+        // some properties are set from INPUT_SERVER, what is probably not so nice
+        // todo: setting it here does not work, whats wrong?
+        //$_SERVER['REMOTE_ADDR'] = '1.1.1.1';
+        //$_SERVER['REQUEST_URI'] = 'some-uri';
+
+        $l = new Logger($this->db);
+        $l->LogMessageWithUserId(LogType::LOG_AUTH_FAILED_NO_SUCH_USER, 101, "testLogMessageWithUserId-msg1");
+
+        // get the id to compare later
+        $id1 = $l->GetLogTypeId(LogType::LOG_AUTH_FAILED_NO_SUCH_USER);
+
+        // read back the values
+        $query = 'SELECT idlog, idlog_type, ts, iduser, historic_user_context, message, request_uri, ip_address, admin_iduser '
+        . 'FROM log_table '
+        . 'ORDER BY idlog DESC';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $this->assertTrue($stmt->rowCount() >= 1);
+        $result = $stmt->fetch();
+        $this->assertNotNull($result);
+        $this->assertSame($id1, $result['idlog_type']);
+        $this->assertNotNull($result['ts']);
+        $this->assertEquals(101, $result['iduser']);
+        $this->assertNotNull($result['historic_user_context']);
+        $this->assertSame('testLogMessageWithUserId-msg1', $result['message']);
+    }       
 }
