170e44e5070d8f940015b53c05739024999c0cfb
Elias Gerber <23149233+eg-be@users.noreply.github.com>
2024-05-22 16:11:50 +0200

    issue #5 PostEntryHandlerTest

--- test/handlers/PostEntryHandlerTest.php
+++ test/handlers/PostEntryHandlerTest.php
@@ -153,9 +153,59 @@ final class PostEntryHandlerTest extends TestCase
         $user->method('NeedsMigration')->willReturn(true);
         $this->db->method('AuthUser')->with('foo', 'bar')->willReturn($user);
 
+        // expect that the logger is called with the correct params when failing
+        $this->logger->expects($this->once())->method('LogMessageWithUserId')
+            ->with(LogType::LOG_OPERATION_FAILED_MIGRATION_REQUIRED, $user);
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage(PostEntryHandler::MSG_MIGRATION_REQUIRED);
         $this->expectExceptionCode(PostEntryHandler::MSGCODE_AUTH_FAIL);
         $this->peh->HandleRequest($this->db);
+    }
+
+    public function testPostEntry_authFailed()
+    {
+        // test that if a user needs migration, the corresponding exception is thrown
+        $_POST[PostEntryHandler::PARAM_PARENTPOSTID] = 0;
+        $_POST[PostEntryHandler::PARAM_NICK] = 'foo';
+        $_POST[PostEntryHandler::PARAM_PASS] = 'bar';
+        $_POST[PostEntryHandler::PARAM_TITLE] = 'abc';
+
+        // fail auth
+        $this->db->method('AuthUser')->willReturn(null);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionCode(PostEntryHandler::MSGCODE_AUTH_FAIL);
+        $this->peh->HandleRequest($this->db);
+    }
+
+    public function testPostEntry_paramValuesStored()
+    {
+        // test that if posting fails, the user-entered values are still available in the handler
+        $_POST[PostEntryHandler::PARAM_PARENTPOSTID] = 777;
+        $_POST[PostEntryHandler::PARAM_NICK] = 'foo';
+        $_POST[PostEntryHandler::PARAM_PASS] = 'bar';
+        $_POST[PostEntryHandler::PARAM_TITLE] = 'abc';
+        $_POST[PostEntryHandler::PARAM_CONTENT] = 'hello wold';
+        $_POST[PostEntryHandler::PARAM_EMAIL] = 'hans@wurst.com';
+        $_POST[PostEntryHandler::PARAM_LINKURL] = 'http://foo.bar.com';
+        $_POST[PostEntryHandler::PARAM_LINKTEXT] = 'foo-bar-link';
+        $_POST[PostEntryHandler::PARAM_IMGURL] = 'https://funny.com/img.jpg';
+
+        // fail auth and read-back the values
+        $this->db->method('AuthUser')->willReturn(null);
+        try 
+        {     
+            $this->peh->HandleRequest($this->db);
+        }
+        catch(InvalidArgumentException $ex) {}
+        
+        $this->assertSame('abc', $this->peh->GetTitle());
+        $this->assertSame('foo', $this->peh->GetNick());
+        $this->assertSame('bar', $this->peh->GetPassword());
+        $this->assertSame('hello wold', $this->peh->GetContent());
+        $this->assertSame('hans@wurst.com', $this->peh->GetEmail());
+        $this->assertSame('http://foo.bar.com', $this->peh->GetLinkUrl());
+        $this->assertSame('foo-bar-link', $this->peh->GetLinkText());
+        $this->assertSame('https://funny.com/img.jpg', $this->peh->GetImgUrl());
     }
 }
